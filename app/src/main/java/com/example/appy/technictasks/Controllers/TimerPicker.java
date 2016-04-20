0af71f79738894c2e50549707e03bcd864a31850
manu92 <emmanuellecoel@yahoo.fr>
2016-04-20 10:51:23 +0200

    Fix TimePicker

--- app/src/main/java/com/example/appy/technictasks/Controllers/TimerPicker.java
+++ app/src/main/java/com/example/appy/technictasks/Controllers/TimerPicker.java
@@ -3,7 +3,6 @@ package com.example.appy.technictasks.Controllers;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -11,8 +10,6 @@ import android.view.View;
 import android.widget.TimePicker;
 
 import com.example.appy.technictasks.R;
-
-import at.markushi.ui.CircleButton;
 
 /**
  * Created by emma on 11/04/2016.
@@ -25,19 +22,12 @@ public class TimerPicker extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //sets the main layout of the activity
         setContentView(R.layout.time_picker);
 
         time_picker = (TimePicker) findViewById(R.id.timePicker);
         time_picker.setIs24HourView(true);
-
-        // set current hour + minute on store time (test with build)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            time_str = time_picker.getHour() + ":" + time_picker.getMinute();
-        } else {
-            time_str = time_picker.getCurrentHour() + ":" + time_picker.getCurrentMinute();
-        }
-
-        // Time change => store time
         time_picker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
             @Override
             public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
@@ -46,7 +36,7 @@ public class TimerPicker extends AppCompatActivity {
         });
 
 
-        finish_btn = (CircleButton) findViewById(R.id.finish);
+        finish_btn = (at.markushi.ui.CircleButton) findViewById(R.id.finish);
         finish_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
