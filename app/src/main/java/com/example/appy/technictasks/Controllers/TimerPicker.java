308cf397b4b4af7527cb534cbba03d07df66480e
manu92 <emmanuellecoel@yahoo.fr>
2016-04-20 16:25:29 +0200

    Fix

--- app/src/main/java/com/example/appy/technictasks/Controllers/TimerPicker.java
+++ app/src/main/java/com/example/appy/technictasks/Controllers/TimerPicker.java
@@ -3,6 +3,7 @@ package com.example.appy.technictasks.Controllers;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -28,6 +29,15 @@ public class TimerPicker extends AppCompatActivity {
 
         time_picker = (TimePicker) findViewById(R.id.timePicker);
         time_picker.setIs24HourView(true);
+
+        // set current hour + minute on store time (test with build)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            time_str = time_picker.getHour() + ":" + time_picker.getMinute();
+        } else {
+            time_str = time_picker.getCurrentHour() + ":" + time_picker.getCurrentMinute();
+        }
+
+        // Time change => store time
         time_picker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
             @Override
             public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
